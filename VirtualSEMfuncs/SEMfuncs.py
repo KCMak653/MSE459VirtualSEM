57f64d267021206e922b78c0e3699bbaf7c6bd93
Kristin Makulowich <k.makulowich@gmail.com>
2020-11-07 12:32:01 -0500

    Added question cell

--- VirtualSEMfuncs/SEMfuncs.py
+++ VirtualSEMfuncs/SEMfuncs.py
@@ -21,3 +21,13 @@ def adjust_contrast(img,vmin, vmax):
     newIm = LUT[arr]
     newIm = np.append(newIm, arrlab, axis=0)
     return(newIm)
+
+def get_values():
+    sampNames = ['Alpha A.R', 'Sulzer A.R', 'Alpha Dry', 'Sulzer Dry', 'Alpha Wet', 'Sulzer Wet']
+    hints = ['Hint 1: Although we would expect the particle size to decrease after ball milling, the same may not be true for particle size',
+            'Hint 2: Look at the EDS - For which milling condition do we expect there to be more contamination?',
+                'Hint 3: What is the key difference between Sulzer and Alpha - how could this affect contamination levels?']
+    answTxt ='Correct answers are shown in red above.  \nYour password to access the SEM images and EDS spectra is: SPEX'
+    answKey = ['D', 'C', 'F', 'B', 'A', 'E']
+    
+    return(hints, sampNames, answTxt, answKey)
